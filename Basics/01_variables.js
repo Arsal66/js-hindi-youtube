ada27676fbf0f21178df2969ef6cb1171f7193dc
Arsal Noor <158061860+Arsal66@users.noreply.github.com>
2025-04-10 15:10:07 +0000

    dataTypes

---
+++ Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144331
+let accountEmail = "ar@gmail.com"
+var accountPassword = "1234"
+accountCity = "Lahore"
+
+//accountId = 123456
+
+
+accountEmail = "ahah@gmail.com"
+accountPassword = "3464"
+accountCity = "islamabad"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
+// we use let instead of var nowadays because var creates the SCOPE problem in javascript before, overriding the same name variables
